5f26c6cdd07681bac38891f7cacd8c25181bc9dc
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-08 12:59:14 -0500

    done with creating classes

--- Lesson 25 - Classes/index.js
+++ Lesson 25 - Classes/index.js
@@ -1,4 +1,21 @@
 function submit(){
     
-    document.getElementById("demo1").innerHTML = "Hello";
+    let student1 = new Student("John", "Smith", 20, "B");
+
+    let student2 = new Student();
+    student2.firstName = "Jane";
+    student2.lastName = "Doe";
+    student2.age = 20;
+    student2.grades = "A+";
+
+    document.getElementById("demo1").innerHTML = student2.firstName;
+}
+
+class Student{
+    constructor(firstName, lastName, age, grades) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grades = grades;
+    }
 }
