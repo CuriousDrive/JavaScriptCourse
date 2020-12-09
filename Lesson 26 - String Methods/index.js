1c7ae7ddaaf96663c2f9e02d9869965a6475394d
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-09 12:48:15 -0500

    getting started with string methods

--- Lesson 26 - String Methods/index.js
+++ Lesson 26 - String Methods/index.js
@@ -1,22 +1,6 @@
 function submit(){
     
-    let student1 = new Student("John", "Smith", 20, "B");
-    document.getElementById("demo1").innerHTML = student1.showGrades();
-}
+    let sentence = "John goes to school everyday";
 
-class Student{
-    constructor(firstName, lastName, age, grades) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.grades = grades;
-    }
-
-    submitAssignment() {
-        return this.firstName + " has submitted the assignment";
-    }
-
-    showGrades() {
-        return this.grades;
-    }
+    document.getElementById("demo1").innerHTML = value;
 }
