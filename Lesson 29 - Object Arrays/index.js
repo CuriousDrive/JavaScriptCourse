ceb128baa57d5c6d6c8cff227c49954f56473425
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-10 14:04:57 -0500

    getting started with onject arrays

--- Lesson 29 - Object Arrays/index.js
+++ Lesson 29 - Object Arrays/index.js
@@ -3,7 +3,5 @@ function submit(){
     let names = ["John", "Jane", "Fahad", "Monica"];
     let scores = [35, 90, 77, 80];
 
-    scores[1] = 50;
-
     document.getElementById("demo1").innerHTML = scores[1];
 }
