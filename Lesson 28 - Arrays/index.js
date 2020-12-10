8af5935c86c40214cb302b7d34b243c561ee0f71
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-10 12:29:11 -0500

    done with arrays

--- Lesson 28 - Arrays/index.js
+++ Lesson 28 - Arrays/index.js
@@ -1,4 +1,9 @@
 function submit(){
     
-    document.getElementById("demo1").innerHTML = "";
+    let names = ["John", "Jane", "Fahad", "Monica"];
+    let scores = [35, 90, 77, 80];
+
+    scores[1] = 50;
+
+    document.getElementById("demo1").innerHTML = scores[1];
 }
