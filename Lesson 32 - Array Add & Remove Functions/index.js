7ff08089806212774b8a005bb0044077939bff43
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-12 09:49:06 -0500

    starting point for push, pop, unshift and shift

---
+++ Lesson 32 - Array Add & Remove Functions/index.js
@@ -0,0 +1,11 @@
+function submit(){
+    
+    let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
+    let scores = [35, 90, 77, 80];
+
+    for (let i = 0; i < scores.length; i++){
+        console.log(scores[i]);
+    }
+
+    document.getElementById("demo1").innerHTML = "";
+}
