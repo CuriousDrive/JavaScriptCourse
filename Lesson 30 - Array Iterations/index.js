fba9c8509d1776f781ba69c6f710bfc1359c632f
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-12 07:31:55 -0500

    redoing the the array for loop

--- Lesson 30 - Array Iterations/index.js
+++ Lesson 30 - Array Iterations/index.js
@@ -3,9 +3,5 @@ function submit(){
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 77, 80];
 
-    for (let i = 0; i < names.length; i++){
-        console.log(names[i]);
-    }
-
-    document.getElementById("demo1").innerHTML = names.length;
+    document.getElementById("demo1").innerHTML = "";
 }
