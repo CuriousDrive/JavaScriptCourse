687be5fec050ae5e47bcdd4f53590d7a91fae9c7
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-12 12:25:30 -0500

    done with push, pop, unshift and shift functions

--- Lesson 32 - Array Add & Remove Functions/index.js
+++ Lesson 32 - Array Add & Remove Functions/index.js
@@ -3,9 +3,14 @@ function submit(){
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 77, 80];
 
+    //scores.push(100);
+    //let lastNumber = scores.pop();
+    //scores.unshift(10);
+    let firstNumber = scores.shift();
+
     for (let i = 0; i < scores.length; i++){
         console.log(scores[i]);
     }
 
-    document.getElementById("demo1").innerHTML = "";
+    document.getElementById("demo1").innerHTML = firstNumber;
 }
