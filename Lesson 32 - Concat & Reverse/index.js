a30164962a19d85ae327ad0c016490648806dfd3
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-16 05:26:24 -0500

    getting ready for concat and reverse arrays

--- Lesson 32 - Concat & Reverse/index.js
+++ Lesson 32 - Concat & Reverse/index.js
@@ -1,8 +1,8 @@
 function submit(){
     
-    let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
+    let names1 = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 77, 80];
-    let moreNames = ["Bill", "Steve", "Satya"];
+    let names2 = ["Bill", "Steve", "Satya"];
 
     for (let i = 0; i < names.length; i++){
         console.log(names[i]);
