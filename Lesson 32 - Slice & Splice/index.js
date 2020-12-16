99c4d1579905d3fa9905195a9f2fa885740734c0
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-16 07:16:19 -0500

    getting started with foreach method

--- Lesson 32 - Slice & Splice/index.js
+++ Lesson 32 - Slice & Splice/index.js
@@ -3,16 +3,9 @@ function submit(){
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 77, 80];
 
-    names.forEach((value,index) => console.log(value + "-" + index));
-    scores.forEach((value,index) => console.log(value + "-" + index));
-
-    // for (let i = 0; i < scores.length; i++){
-    //     console.log(scores[i]);
-    // }
-
-    // for (let i = 0; i < names.length; i++){
-    //     console.log(names[i]);
-    // }
-
+    for (let i = 0; i < scores.length; i++){
+        console.log(scores[i]);
+    }
+    
     document.getElementById("demo1").innerHTML = "";
 }
