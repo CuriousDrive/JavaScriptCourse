391af108b8ba821e778977751271b478c782c6de
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-16 06:20:38 -0500

    done with slice and splice

--- Lesson 32 - Slice & Splice/index.js
+++ Lesson 32 - Slice & Splice/index.js
@@ -3,6 +3,9 @@ function submit(){
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 77, 80];
 
+    //let results = names.slice(2, 5);
+    names.splice(2, 3, "Bill", "Steve", "Satya");
+
     for (let i = 0; i < names.length; i++){
         console.log(names[i]);
     }
