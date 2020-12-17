7dd8ede08b0018f23655cd16f239225f3574991b
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-17 18:02:27 -0500

    starting point of filer and map methods

---
+++ Lesson 34 - Filter & Maps/index.js
@@ -0,0 +1,9 @@
+function submit() {
+    
+    let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
+    let scores = [35, 90, 50, 80];
+    
+    scores.forEach((value) => console.log(value));
+    
+    document.getElementById("demo1").innerHTML = "";
+}
