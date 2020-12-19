c45b7b76a3c651f330dd2b80efef89c3df7bbf59
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-19 07:35:01 -0500

    starting point of filter method

--- Lesson 35 - Array Filter/index.js
+++ Lesson 35 - Array Filter/index.js
@@ -3,17 +3,10 @@ function submit() {
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 50, 80];
     
-    //scores.forEach((value,index,score) => scores[index] = value*100);
-    scores.forEach((value) => console.log(value));
-
-    console.log("---------");
-
     let goodScores = scores.map((value) => value = value + 3);
     goodScores.forEach((value) => console.log(value));
 
-    console.log("---------");
-    scores.forEach((value) => console.log(value));
+    console.log("----------------");
 
-    
     document.getElementById("demo1").innerHTML = "";
 }
