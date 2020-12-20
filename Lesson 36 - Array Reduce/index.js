16bd2feb0947a990487154e2edad204fb7151b3c
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-20 08:20:05 -0500

    done with reduce

--- Lesson 36 - Array Reduce/index.js
+++ Lesson 36 - Array Reduce/index.js
@@ -2,6 +2,8 @@ function submit() {
     
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 50, 80];
+
+    let sum = names.reduceRight((prev, curr) => prev + curr);
     
-    document.getElementById("demo1").innerHTML = "";
+    document.getElementById("demo1").innerHTML = sum;
 }
