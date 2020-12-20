c419c35d7435312e02e284cff0477e700e962cbd
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-20 09:08:02 -0500

    starting point of sort

--- Lesson 37 - Array Sort/index.js
+++ Lesson 37 - Array Sort/index.js
@@ -3,5 +3,7 @@ function submit() {
     let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
     let scores = [35, 90, 50, 80];
 
+    names.forEach((value) => console.log(value));
+    
     document.getElementById("demo1").innerHTML = "";
 }
