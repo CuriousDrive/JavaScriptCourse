329addf4dbcea54098711634e19fcaea1a307f89
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-20 09:06:01 -0500

    starting point of sort method

---
+++ Lesson 37 - Array Sort/index.js
@@ -0,0 +1,7 @@
+function submit() {
+    
+    let names = ["John", "Jane", "Fahad", "Monica", "Sherlock"];
+    let scores = [35, 90, 50, 80];
+
+    document.getElementById("demo1").innerHTML = "";
+}
