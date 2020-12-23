6655ad5c024ee6b4b5c98511e127c7090c9302a3
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-12-23 08:00:11 -0500

    done with Math Object 2

--- Lesson 41 - Math Object - 2/index.js
+++ Lesson 41 - Math Object - 2/index.js
@@ -1,5 +1,10 @@
 function submit() {
     
-    let result = 11;
+    //let result = Math.pow(11, 3); //11*11*11 => 1331
+    //let result = Math.sqrt(121);
+    //let result = Math.PI*Math.pow(11,2);
+    //let result = Math.min(22, 56, 8);
+    let result = Math.max(22, 56, 8, 99);
+    
     document.getElementById("demo1").innerHTML = result;
 }
